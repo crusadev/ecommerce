78a36d44fab071e2b6fce2c65e0d358bb1a29747
2nio <antoniogeorgetoader@yahoo.ro>
2024-10-08 06:33:22 -0700

    server update

--- backend/server.js
+++ backend/server.js
@@ -1,51 +1,23 @@
-// npm run dev
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const multer = require('multer')
 
-require('dotenv').config();
-
-const express = require('express');
-const expressLayout = require('express-ejs-layouts');
-const methodOverride= require('method-override');
-const cookieParser=require('cookie-parser');
-const session = require('express-session');
-const MongoStore= require('connect-mongo');
-
-const connectDB=require('./server/config/db');
-const { isActiveRoute }=require('./server/helpers/routeHelpers');
-
-const app= express();
+const app = express()
 const PORT= 8080 || process.env.PORT;
- 
-//Connect to DB
-connectDB();
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-app.use(cookieParser());
-app.use(methodOverride('_method'));
-
-
-app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true,
-    store: MongoStore.create({
-         mongoUrl: process.env.mongodb_URI
-    }),
-   //cookie: {maxAge: new Date (Date.now() + (3600000) ) }
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
 }))
+app.use(express.json({ limit: '50mb' }))
+app.use(cookieParser())
 
-app.use(express.static('public'));
+mongoose.connect(`mongodb+srv://${process.env.mongoPASS}:zfwB5USIUvKplQjv@cluster0.bezcq.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0`)
+.then(result => console.log("Connected to mongodb"))
 
-// Templating Engine
-app.use(expressLayout);
-app.set('layout','./layouts/main');
-app.set('view engine', 'ejs');
-
-app.locals.isActiveRoute= isActiveRoute;
-
-
-app.use('/',require('./server/routes/main'));
-app.use('/',require('./server/routes/admin'));
 
 app.listen(PORT, () => {
     console.log(`APP listening on port ${PORT}`);
